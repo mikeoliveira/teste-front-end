0c366a40b9f6e08764da4155d9c0f92458e60d90
Mike Oliveira Costa <mocosta@uolinc.com>
2018-10-08 22:00:36 -0300

    Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ListComponent } from './list/list.component';
+import { DetailsComponent } from './details/details.component';
+
+const routes: RouterModule[] = [
+  { path: 'list', component: ListComponent },
+  { path: 'details', component: DetailsComponent },
+  { path: '', redirectTo: '/list', pathMatch: 'prefix' }
+ ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ListComponent,
+    DetailsComponent
   ],
   imports: [
-    BrowserModule, FormsModule ,HttpClientModule
+    BrowserModule, FormsModule ,HttpClientModule, RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
